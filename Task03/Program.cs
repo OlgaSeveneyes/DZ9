c3689a2705ae4d2f437a1507e44da09f11933d65
Olga <osa_007@mail.ru>
2022-09-09 14:12:41 +0300

    added task3 for dz9

---
+++ Task03/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+Console.Clear();
+int m = NumberFromUser ("Введите число m: ","Ошибка ввода!");
+int n = NumberFromUser ("Введите число n: ","Ошибка ввода!");
+AckermannFunction (m, n);
+Console.WriteLine($"Функция Аккермана A({m},{n}) = {AckermannFunction(m, n)}");
+
+// возвращает число, введенное пользователем, либо сообщение об ошибке
+
+int NumberFromUser (string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+// возвращает функцию Аккермана
+
+int AckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+    return AckermannFunction(numberM, numberN);
+}
